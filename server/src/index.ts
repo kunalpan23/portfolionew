01e57982aaf5c89e97dd0f10ec2a823793c9f9bc
Kunal Panchal <kunalpanchal@Kunals-Macbook-Air.local>
2022-03-29 12:02:23 +0530

    server created

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,27 @@
 import express from "express";
 import dotenv from 'dotenv';
-
+import cors from 'cors'; 
+import path from 'path';
+import { FILE_NAME} from './docs';
 dotenv.config();
 const app = express();
 const port = 3000;
 
+
+app.use(cors());
+
 app.get("/", (req, res) => {
-  res.send("You are listening from express server");
+  res.send("Server Connected!");
+});
+
+app.get("/download", function(req, res){
+  const fileName = "kunalresume.pdf";
+
+  res.download(path.join(__dirname, `/docs/${FILE_NAME}`), fileName, (err) => {
+    if(err){
+      console.log("Error@Download: ", err);
+    }
+  });
 });
 
 app.listen(port, (err?) => {
